24e5b64738426fdab7cd33293135a757cbed0738
Duinaart <64527903+Duinaart@users.noreply.github.com>
2020-10-06 18:18:54 +0200

    Add files via upload

---
+++ financial_dashboard.py
@@ -0,0 +1,135 @@
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+
+import pandas as pd
+import requests
+
+import plotly.graph_objects as go
+import plotly.express as px
+
+from dash.dependencies import Input, Output, State
+
+app = dash.Dash(external_stylesheets=[dbc.themes.DARKLY])
+##############################################################
+
+
+###############################################################
+# Make a navigation bar (dropdown items)
+## Make a reusable navitem for different dashboards
+nav_item = dbc.NavItem(
+    dbc.Row(
+        [dbc.NavLink('Euronext Stock Exchange', href='https://live.euronext.com/nl'),
+        dbc.NavLink('Yahoo finance', href='https://finance.yahoo.com/')
+         ],
+        align='center',
+    ),
+
+)
+
+## Make a Reusable dropdown for different dashboards
+dropdown = dbc.Col(
+    dbc.DropdownMenu(
+        children=[
+            dbc.DropdownMenuItem('Dash Documentation', href='https://dash.plotly.com/'),
+            dbc.DropdownMenuItem('Bootstrap Documentation', href='https://dash-bootstrap-components.opensource.faculty.ai/docs/'),
+            dbc.DropdownMenuItem('Quandl Documentation', href='https://docs.quandl.com/'),
+            dbc.DropdownMenuItem(divider=True),
+            dbc.DropdownMenuItem('Youtube tutorial', href='https://www.youtube.com/watch?v=P-XYio7G_Dg&ab_channel=PipInstallPython')
+        ],
+        nav=True,       # needs to be set to true if it is inside navigation bar to get consistent styling
+        in_navbar=True,  # say that it needs to be in navigation bar
+        label= 'Useful Links',
+        right=True,
+    ),
+    width='auto'
+    )
+
+
+## Navigation Bar layout
+navbar = dbc.Navbar(
+    # dbc.Container(      # A container keeps the content of the rows and columns into one single blob that can be moved
+        [
+            html.A(
+                # Use row and col to control vertical alignment of logo
+                dbc.Row(
+                    [dbc.Col((html.Img(src='https://prismic-io.s3.amazonaws.com/plotly-marketing-website/bd1f702a-b623-48ab-a459-3ee92a7499b4_logo-plotly.svg',height='30px')), width='auto'),
+                    dbc.Col((dbc.NavbarBrand('Finance Dashboard')), width='auto')],
+                    align='center',
+                    no_gutters=True,
+                ),
+            ),
+            dbc.NavbarToggler(id='navbar-toggler2'),
+            dbc.Collapse(
+                dbc.Nav(
+                        dbc.Col(
+                            [nav_item,
+                            dropdown,
+                            ], width='auto'), className='ml-auto', navbar=True,
+                ),
+                id='navbar-collapse2',
+                navbar=True,
+           ),
+        ],
+    # ),
+color='dark',
+dark=True,
+className='mb-5',)
+
+#########################################
+app.layout = html.Div(
+    [navbar,
+     html.H2('Financial Dashboard for Euronext Stock Exchange', style={'text-align':'center'}),
+
+     html.Div(
+         [
+             dbc.Input(id="input", placeholder='Insert ticker on Euronext', type='text'),
+             html.Br(),
+             html.P(id="output"),
+         ]
+     ),
+     html.Div(id='linegraph-container',),
+     html.Br(),
+     html.Div(id='candlestick-container')
+     ]
+)
+
+###############################################################
+# !Figure out how to combine multiple outputs so that i can get a linegraph and candlestick chart under input box
+@app.callback(Output('output', 'children'), [Input('input', 'value')])
+
+def get_company_name(value):
+    response = requests.get('https://www.quandl.com/api/v3/datasets/EURONEXT/{}.json?api_key=1eCS2saTbHTFds2LjKkX'.format(value))
+    dataset = response.json()
+    company_name = dataset['dataset']['name']
+    return company_name
+
+
+@app.callback(
+    [Output(component_id='linegraph-container', component_property='children'),
+    Output(component_id='candlestick-container', component_property='children')],
+    [Input(component_id='input', component_property='value')])
+
+def get_ohlc_df(value):
+    response = requests.get('https://www.quandl.com/api/v3/datasets/EURONEXT/{}.json?api_key=1eCS2saTbHTFds2LjKkX'.format(value))
+    json_file = response.json()
+    data = json_file['dataset']['data']
+    df = pd.DataFrame(data, columns=['date', 'open', 'high', 'low', 'close', 'volume', 'turnover'])
+    df = df[::-1]
+
+    fig = px.line(df['close'], title='Stock price evolution')
+    fig2 = go.Figure(data=[go.Candlestick(x=df['date'],
+                open=df['open'],
+                high=df['high'],
+                low=df['low'],
+                close=df['close'])])
+    return dcc.Graph(id='linegraph-container',
+                figure=fig
+                ),dcc.Graph(id='candlestick-container',
+                figure=fig2)
+
+
+#################################################################
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8000,)
