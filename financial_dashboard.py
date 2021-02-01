a64eb6da16c84799c02cee67ba67076816938820
Duinaart <64527903+Duinaart@users.noreply.github.com>
2021-02-01 20:21:31 +0100

    Update financial_dashboard.py

--- financial_dashboard.py
+++ financial_dashboard.py
@@ -1,13 +1,12 @@
 import dash
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
 from dash.dependencies import Input, Output
 
+import json
 import pandas as pd
 
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import plotly.express as px
 
 import requests
 from bs4 import BeautifulSoup as bs
@@ -34,47 +33,42 @@ app.layout = tabs
 '''CALLBACKS EURONEXT TAB'''
 
 
-@app.callback(Output('output', 'children'), [Input('input', 'value')])
-def get_company_name(value):
-    if value is not None:
-        response = requests.get(
-            'https://www.quandl.com/api/v3/datasets/EURONEXT/{}.json?api_key=1eCS2saTbHTFds2LjKkX'.format(value))
-        dataset = response.json()
-        company_name = dataset['dataset']['name']
-        return company_name
-
-
-@app.callback(Output('output2', 'children'), [Input('input', 'value')])
-def get_latest_close(value):
-    if value is not None:
-        response = requests.get(
-            'https://www.quandl.com/api/v3/datasets/EURONEXT/{}.json?api_key=1eCS2saTbHTFds2LjKkX'.format(value))
-        dataset = response.json()
-        latest_close = dataset['dataset']['data'][0][4]
-        return 'The latest close is: ' + str(latest_close)
-
-
-@app.callback(Output('output3', 'children'), [Input('input', 'value')])
-def get_market(value):
+# Write callback with memory to only take 1 API call and reuse it for the other calbacks
+@app.callback(Output('memory-output', 'data'),[Input('input', 'value')])
+def store_json_data(value):
     if value is not None:
         response = requests.get(
             'https://www.quandl.com/api/v3/datasets/EURONEXT/{}.json?api_key=1eCS2saTbHTFds2LjKkX'.format(value))
         data = response.json()
-        dataset = data['dataset']['description']
-        split_dataset = dataset.split("<br>")
-        market = split_dataset[2]
-        return market
+        return data
+
+
+@app.callback(Output('output', 'children'), [Input('memory-output', 'data')])
+def get_company_name(data):
+    company_name = data['dataset']['name']
+    return company_name
+
+
+@app.callback(Output('output2', 'children'), [Input('memory-output', 'data')])
+def get_latest_close(data):
+    latest_close = data['dataset']['data'][0][4]
+    return 'The latest close is: ' + str(latest_close)
+
+
+@app.callback(Output('output3', 'children'), [Input('memory-output', 'data')])
+def get_market(data):
+    dataset = data['dataset']['description']
+    split_dataset = dataset.split("<br>")
+    market = split_dataset[2]
+    return market
 
 
 @app.callback(
     Output('linegraph-container-enx', 'figure'),
-    [Input('input', 'value')])
-def update_graph(value):
-    response = requests.get(
-        'https://www.quandl.com/api/v3/datasets/EURONEXT/{}.json?api_key=1eCS2saTbHTFds2LjKkX'.format(value))
-    json_file = response.json()
-    data = json_file['dataset']['data']
-    df = pd.DataFrame(data, columns=['date', 'open', 'high', 'low', 'close', 'volume', 'turnover'])
+    [Input('memory-output', 'data')])
+def update_graph(data):
+    dataset = data['dataset']['data']
+    df = pd.DataFrame(dataset, columns=['date', 'open', 'high', 'low', 'close', 'volume', 'turnover'])
     df = df[::-1]
 
     fig = make_subplots(rows=2, cols=1, shared_xaxes=True)
@@ -86,7 +80,7 @@ def update_graph(value):
                              mode='lines', line=dict(color='#456987')), row=1, col=1)
     fig.add_trace(go.Bar(x=df['date'], y=df['volume'],
                          hovertemplate='Volume: %{y}' +
-                                       '<extra></extra>', marker_color='#184a1a'), row=2, col=1)
+                                       '<extra></extra>', marker=dict(color='#184a1a')), row=2, col=1)
 
     # Add the layout to the subplots
     fig.layout.update(
@@ -103,8 +97,6 @@ def update_graph(value):
     fig['layout']['yaxis']['title'] = 'Price'
     fig['layout']['yaxis2']['title'] = 'Volume'
     return fig
-
-
 
 
 #########################################################################################################
@@ -128,18 +120,17 @@ def full_company_name_sp(value):
 @app.callback(Output('outputsp', 'children'), [Input('inputsp', 'value')])
 def get_latest_close_sp(value):
     if value is not None:
-        data = si.get_data(value, start_date='02/01/2017', index_as_date=True, interval='1d')
-        df = pd.DataFrame(data)
+        dataset = si.get_data(value, start_date='02/01/2017', index_as_date=True, interval='1d')
+        df = pd.DataFrame(dataset)
         df = df[::-1]
         latest_close = round(df.loc[df.index[0], 'adjclose'], 2)
         return 'The latest close is: ' + str(latest_close)
-    else:
-        return ''
+
 
 
 @app.callback(
-    Output(component_id='linegraph-container-sp', component_property='figure'),
-    [Input(component_id='inputsp', component_property='value')])
+    Output('linegraph-container-sp', 'figure'),
+    [Input('inputsp', 'value')])
 def update_graph(value):
     data = si.get_data(value, start_date='02/01/2017', index_as_date=True, interval='1d')
     df = pd.DataFrame(data)
